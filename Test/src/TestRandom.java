f37d6d31b9d56317e0a5da3bdae033b373709e5b
Billy Krajcovic <wjkrajco@ncsu.edu>
2022-04-13 10:55:55 -0400

    Testing

---
+++ Test/src/TestRandom.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.*;
+
+public class TestRandom {
+
+    public static void main(String[] args)  {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a seed for the dice roll");
+        int seed = -1;
+        seed = in.nextInt();
+        Random rand = new Random(seed);
+        for (int i = 0; i < 3; ++i)
+        System.out.print(rand.nextInt(7));
+    }
+}
